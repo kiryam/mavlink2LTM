8de4529bce85d5a78ceef311d24a4b821c762c53
Kirill Ivannikov <kiryam@kiryam.ru>
2018-12-28 23:15:01 +0300

    initial

---
+++ cmd/mavlink2ltm/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jacobsa/go-serial/serial"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/mavlink"
+	common "gobot.io/x/gobot/platforms/mavlink/common"
+	"log"
+)
+
+func main() {
+	var mavlinkUDP, serialPort string
+	var serialBaud int
+
+	flag.StringVar(&mavlinkUDP, "mavlink", "127.0.0.1:14550", "Example: -mavlink 127.0.0.1:14550")
+	flag.StringVar(&serialPort, "serial", "", "Example: -serial /dev/cu.SLAB_USBtoUART")
+	flag.IntVar(&serialBaud, "baud", 2400, "Example: -baud 2400")
+	flag.Parse()
+
+	if serialPort == ""{
+		panic("Set -serial (-help for help)")
+	}
+
+	fmt.Println(fmt.Sprintf("Converting from %s to %s at %dbaud", mavlinkUDP, serialPort, serialBaud))
+
+	adaptor := mavlink.NewUDPAdaptor(mavlinkUDP)
+	iris := mavlink.NewDriver(adaptor)
+
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        serialPort,
+		BaudRate:        2400,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	work := func() {
+		iris.On(mavlink.MessageEvent, func(data interface{}) {
+			if data.(common.MAVLinkMessage).Id() == 24 {
+				message := data.(*common.GpsRawInt)
+				fmt.Println(fmt.Sprintf("Sent %d, %d ALT %d", message.LAT, message.LON, message.ALT))
+
+				crc := byte(0x00)
+				payload := []byte{ /*LAT*/ 0x00, 0x00, 0x00, 0x00 /*LON*/, 0x00, 0x00, 0x00, 0x00 /*Ground Speed*/, 0x00 /*Altitude*/, 0x00, 0x00, 0x00, 0x00 /*Sats*/, 0x00}
+				payload[0] = byte((message.LAT >> (8 * 0))  & 0xff)
+				payload[1] = byte((message.LAT >> (8 * 1))  & 0xff)
+				payload[2] = byte((message.LAT >> (8 * 2))  & 0xff)
+				payload[3] = byte((message.LAT >> (8 * 3))  & 0xff)
+
+				payload[4] = byte((message.LON >> (8 * 0))  & 0xff)
+				payload[5] = byte((message.LON >> (8 * 1))  & 0xff)
+				payload[6] = byte((message.LON >> (8 * 2))  & 0xff)
+				payload[7] = byte((message.LON >> (8 * 3))  & 0xff)
+
+				// TODO: ground speed
+
+				payload[8] = byte((message.ALT >> (8 * 0))  & 0xff)
+				payload[9] = byte((message.ALT >> (8 * 1))  & 0xff)
+				payload[10] = byte((message.ALT >> (8 * 2))  & 0xff)
+				payload[11] = byte((message.ALT >> (8 * 3))  & 0xff)
+
+				// TODO: sats
+
+				for i := 0; i < len(payload); i++ {
+					crc ^= payload[i]
+				}
+
+				b := []byte{0x24, 0x54, 0x47}
+				b = append(b, payload...)
+				b = append(b, crc)
+				_, err := port.Write(b)
+				if err != nil {
+					log.Fatalf("port.Write: %v", err)
+				}
+
+			}
+		})
+	}
+
+	robot := gobot.NewRobot("mavBot",
+		[]gobot.Connection{adaptor},
+		[]gobot.Device{iris},
+		work,
+	)
+
+	robot.Start()
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module ltmproxy
+
+require (
+	github.com/creack/goselect v0.0.0-20180501195510-58854f77ee8d // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/jacobsa/go-serial v0.0.0-20180131005756-15cf729a72d4
+	go.bug.st/serial.v1 v0.0.0-20180827123349-5f7892a7bb45 // indirect
+	gobot.io/x/gobot v1.12.0
+	golang.org/x/sys v0.0.0-20181221143128-b4a75ba826a6 // indirect
+)
